603b645376e5a9affa5f2ccb794389bc0d01795a
marckysan <e0425377@u.nus.edu>
2022-09-16 08:30:11 +0800

    WIP heroku testing

--- app.js
+++ app.js
@@ -9,16 +9,30 @@ const PORT = process.env.PORT || 5000;
 const app = express().use(express.json());
 morganBody(app, { noColors: process.env.NODE_ENV === "production" });
 
+app.get("/test/get", (req, res) => {
+  res.send("Get Endpoint is working");
+});
+
+app.post("/test/post", (req, res) => {
+  res.send("Post Endpoint is working");
+});
+
 app.post("/tickerStreamPart1", (req, res) => {
   const { stream } = req.body;
   const output = to_cumulative(stream);
-  res.json(output);
+  const wrappedOutput = {
+    output: output,
+  };
+  res.json(wrappedOutput);
 });
 
 app.post("/tickerStreamPart2", (req, res) => {
   const { stream, quantityBlock } = req.body;
   const output = to_cumulative_delayed(stream, quantityBlock);
-  res.json(output);
+  const wrappedOutput = {
+    output: output,
+  };
+  res.json(wrappedOutput);
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));

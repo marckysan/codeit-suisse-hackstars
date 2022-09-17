54c58e9dbbf7b1f75d1506aa4f3af5ce820e58b2
timltj <timothylim1413@gmail.com>
2022-09-17 18:43:42 +0800

    WIP TSR

--- src/travelling_suisse_robot/travelling_suisse_robot.js
+++ src/travelling_suisse_robot/travelling_suisse_robot.js
@@ -1,8 +1,12 @@
+const e = require("express");
+const { shortestPath } = require("../utils/utils.js");
+
 exports.travelling_suisse_robot = (input) => {
     let inputItems = [...input];
 
     let graph = [];
     let row = [];
+    let map = new Map();
 
     // fill graph
     for (let i = 0; i < inputItems.length; i++) {
@@ -14,4 +18,23 @@ exports.travelling_suisse_robot = (input) => {
         }
         row.push(inputItems[i]);
     }
+
+    // fill Map
+    for (let row = 0; row < graph.length; row++) {
+        for (let col = 0; col < graph[row].length; col++) {
+            let element = graph[row][col];
+
+            if (element !== "\\" && element !== "n" && element !== " ") {
+                if (!map.has(element)) {
+                    map.set(element, [row, col])
+                } else {
+                    map.set(element + row + col, [row, col]) // duplicate characters
+                }
+            }
+        }
+    }
+
+    // find path
+
+    console.log(map)
 }

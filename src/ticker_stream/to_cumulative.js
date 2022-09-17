7a0be9894cbf7ec10b415dd1434547299c8eaaf3
marckysan <e0425377@u.nus.edu>
2022-09-17 10:08:50 +0800

    Removed comments

--- src/ticker_stream/to_cumulative.js
+++ src/ticker_stream/to_cumulative.js
@@ -1,42 +1,5 @@
 const { sortTicks, generateTickerMap } = require("../utils/utils.js");
 
-// exports.to_cumulative = (ticks) => {
-//   // Sort the tickers alphabetically
-//   sortedTicks = sortTicks(ticks);
-//   const comma = ",";
-//   let outputString = "";
-//   let currentTick = "";
-//   let currentTime = "";
-
-//   // Replace the ticker price with the cumulative value and build the string
-//   for (let i = 0; i < ticks.length; i++) {
-//     const item = sortedTicks[i];
-//     const tickDetails = item.split(",");
-//     const timeStamp = tickDetails[0];
-//     const tick = tickDetails[1];
-//     const quantity = tickDetails[2];
-//     const price = tickDetails[3];
-//     const cNotional = quantity * price;
-
-//     if (timeStamp !== currentTime) {
-//       outputString +=
-//         timeStamp + comma + tick + comma + quantity + comma + cNotional;
-//       if (i < sortedTicks.length - 1) {
-//         outputString += comma;
-//       }
-//     } else {
-//       outputString += tick + comma + quantity + comma + cNotional;
-//       if (i < sortedTicks.length - 1) {
-//         outputString += comma;
-//       }
-//     }
-//     currentTick = tick;
-//     currentTime = timeStamp;
-//   }
-
-//   return [outputString];
-// };
-
 exports.to_cumulative = (ticks) => {
   // Sort the tickers alphabetically
   const sortedTicks = sortTicks(ticks);
@@ -76,34 +39,6 @@ exports.to_cumulative = (ticks) => {
         cNotional: cNotional,
       };
       tickerMap[tick] = updateTickerRef;
-
-      //   for (const [key] of Object.entries(tickerMap)) {
-      //     outputString +=
-      //       timeStamp +
-      //       comma +
-      //       key +
-      //       comma +
-      //       tickerMap[key].cQuantity +
-      //       comma +
-      //       tickerMap[key].cNotional;
-
-      //     if (i < sortedTicks.length - 1) {
-      //       outputString += comma;
-      //     }
-      //     let tickerMapRefNew = {
-      //       time: timeStamp,
-      //       cQuantity: 0,
-      //       cNotional: 0.0,
-      //     };
-
-      //     tickerMap[key] = tickerMapRefNew;
-      //   }
-      //   let newTimeStampTickerRef = {
-      //     time: timeStamp,
-      //     cQuantity: newQuantity,
-      //     cNotional: newCNotional,
-      //   };
-      //   tickerMap[tick] = newTimeStampTickerRef;
     } else {
       // Update the ticker ref
       let updateTickerRef = {

3b5b760d6e3715dc28c9931d58de73bbe1b8115d
natashasoon <natashasoon@u.nus.edu>
2022-09-17 15:46:46 +0800

    reverting to original solution

--- src/cryptocollapz/cryptocollapz.js
+++ src/cryptocollapz/cryptocollapz.js
@@ -10,18 +10,17 @@ maximum = (x) => {
 };
 
 exports.cryptocollapz = (input) => {
-  output = input;
-  //   output = [];
-  //   for (let j = 0; j < input.length; j++) {
-  //     x = input[j];
-  //     outputInternal = [];
-  //     for (let i = 0; i < x.length; i++) {
-  //       let num = parseInt(x[i]);
-  //       let v = maximum(num);
-  //       // output.push(i);
-  //       outputInternal.push(v);
-  //     }
-  //     output.push(outputInternal);
-  //   }
+  output = [];
+  for (let j = 0; j < input.length; j++) {
+    x = input[j];
+    outputInternal = [];
+    for (let i = 0; i < x.length; i++) {
+      let num = parseInt(x[i]);
+      let v = maximum(num);
+      // output.push(i);
+      outputInternal.push(v);
+    }
+    output.push(outputInternal);
+  }
   return output;
 };
